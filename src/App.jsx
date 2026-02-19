4201244678c2f51005f2f1d1efecda34d0a60a1b
khadija-Noori <nargessadat617@gmail.com>
2026-02-20 00:02:27 +0430

    make my app

--- src/App.jsx
+++ src/App.jsx
@@ -37,24 +37,82 @@ useEffect(() => {
   fetchCountries();
 }, []);
 
+//  make a new use effet for filtering our data
+useEffect(() => {
+  const fetchCountries = async () => {
+    try {
+      setLoading(true);
+      setError(null);
 
-  return (
-    <div>
-      <h1>Countries Explorer</h1>
-       {loading && <p>Loading countries...</p>}
-      {error && <p>Error: {error}</p>}
-    {/* in here i added search filter component */}
-  <SearchFilter
-  search={search}
-  setSearch={setSearch}
-  region={region}
-  setRegion={setRegion}
-  />
-  <CountryList countries={countries} />
-  <CountryList countries={countries} />
+      let url = "";
 
+      if (search.length >= 2) {
+        url = `https://restcountries.com/v3.1/name/${encodeURIComponent(search)}?fields=name,flags,region,population`;
+      } else if (region !== "all") {
+        url = `https://restcountries.com/v3.1/region/${encodeURIComponent(region)}?fields=name,flags,region,population`;
+      } else {
+        url = "https://restcountries.com/v3.1/all?fields=name,flags,region,population";
+      }
+
+      const res = await fetch(url);
+      if (!res.ok) {
+        throw new Error("Failed to fetch countries");
+      }
+
+      const data = await res.json();
+      setCountries(data);
+    } catch (err) {
+      setError(err.message);
+      setCountries([]);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  fetchCountries();
+}, [search, region]);
+
+
+ return (
+  <div>
+    <h1>Countries Explorer</h1>
+
+    {/* Show Loading */}
+    {loading && <p>Loading countries...</p>}
+
+    {/* Show Error and Retry button */}
+    {error && (
+      <div>
+        <p>Error: {error}</p>
+        <button
+          onClick={() => {
+            setSearch(""); 
+            setRegion("all"); 
+            setCountries([]);
+          }}
+        >
+          Retry
+        </button>
+      </div>
+    )}
+
+    {/* Search and Region filter controls */}
+    <SearchFilter
+      search={search}
+      setSearch={setSearch}
+      region={region}
+      setRegion={setRegion}
+    />
+
+    {/* Show "No results found" when countries array is empty */}
+    {!loading && !error && countries.length === 0 && <p>No results found</p>}
+
+    {/* Show the list of countries */}
+    {!loading && !error && countries.length > 0 && (
+      <CountryList countries={countries} />
+    )}
   </div>
-  );
-  }
+);
 
+}
 export default App;
